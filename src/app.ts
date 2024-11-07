92d6cd62060759eb3a62265c76cf1e746ef92494
Arpan Lahiri <arpanlahiri@plexusmd.com>
2024-11-08 01:37:14 +0530

    Middleware updated

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
@@ -26,8 +29,5 @@ app.get<{}, MessageResponse>('/', (req, res) => {
 });
 
 app.use('/api/', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 export default app;
